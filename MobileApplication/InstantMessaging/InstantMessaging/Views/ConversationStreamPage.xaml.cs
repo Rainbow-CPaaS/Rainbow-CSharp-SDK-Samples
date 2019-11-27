3cf98c023d9dfbd492ce08ccb43e4aacc8dc0369
Christophe Irles <christophe.irles@al-enterprise.com>
2019-11-27 17:22:17 +0100

    ConversationStreamPage: avoid item selection and scroll to bottom when page is appearing

--- MobileApplication/InstantMessaging/InstantMessaging/Views/ConversationStreamPage.xaml.cs
+++ MobileApplication/InstantMessaging/InstantMessaging/Views/ConversationStreamPage.xaml.cs
@@ -16,6 +16,8 @@ namespace InstantMessaging
 
         private String peerId;
 
+        private Boolean needToScrollAtBottom = false;
+
         public ConversationStreamPage(String peerId)
         {
             this.peerId = peerId;
@@ -24,6 +26,59 @@ namespace InstantMessaging
 
             vm = new ConversationStreamViewModel(peerId);
             BindingContext = vm;
+
+            this.Appearing += ConversationStreamPage_Appearing;
+
+            
+            MessagesListView.ItemAppearing += MessagesListView_ItemAppearing;
+
         }
+
+        #region EVENTS FIRED ON THIS CONTENT PAGE
+
+        private void MessagesListView_ItemAppearing(object sender, ItemVisibilityEventArgs e)
+        {
+            base.OnAppearing();
+
+            if (needToScrollAtBottom)
+            {
+                if (vm.MessagesList.Count > 0)
+                {
+                    Device.BeginInvokeOnMainThread(() =>
+                    {
+                        try
+                        {
+                            MessagesListView.ScrollTo(vm.MessagesList[vm.MessagesList.Count - 1], ScrollToPosition.MakeVisible, false);
+                            needToScrollAtBottom = false;
+                        }
+                        catch
+                        {
+                        }
+
+                    });
+                }
+            }
+        }
+
+        private void ConversationStreamPage_Appearing(object sender, EventArgs e)
+        {
+            needToScrollAtBottom = true;
+        }
+
+#endregion EVENTS FIRED ON THIS CONTENT PAGE
+
+#region EVENTS FIRED BY XAML
+
+        void MessagesListView_OnItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            MessagesListView.SelectedItem = null;
+        }
+
+        void MessagesListView_OnItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            MessagesListView.SelectedItem = null;
+        }
+
+#endregion EVENTS FIRED BY XAML
     }
 }
