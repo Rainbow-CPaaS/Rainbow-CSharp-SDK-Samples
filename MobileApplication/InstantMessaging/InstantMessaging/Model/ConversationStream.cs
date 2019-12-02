9ca141fcb806ed6a6605aa3d50ffaf96b0fee147
Christophe Irles <christophe.irles@al-enterprise.com>
2019-12-02 17:19:30 +0100

    Add missing file

---
+++ MobileApplication/InstantMessaging/InstantMessaging/Model/ConversationStream.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using MvvmHelpers;
+
+using Xamarin.Forms;
+
+namespace InstantMessaging.Model
+{
+    public class ConversationStream : ObservableObject
+    {
+        String loadingIndicatorIsVisibleheaderIsVisible;
+        String listViewIsEnabled;
+
+        public string LoadingIndicatorIsVisible
+        {
+            get { return loadingIndicatorIsVisibleheaderIsVisible; }
+            set { SetProperty(ref loadingIndicatorIsVisibleheaderIsVisible, value); }
+        }
+
+        public string ListViewIsEnabled
+        {
+            get { return listViewIsEnabled; }
+            set { SetProperty(ref listViewIsEnabled, value); }
+        }
+    }
+}
