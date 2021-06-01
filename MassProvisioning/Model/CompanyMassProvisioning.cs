d043634f8eded849173141140ffce62d88b12382
Christophe Irles <christophe.irles@al-enterprise.com>
2021-06-01 11:32:19 +0200

    [MassProvisioning] add missing files

---
+++ MassProvisioning/Model/CompanyMassProvisioning.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MassProvisioning.Model
+{
+    public class CompanyMassProvisioning
+    {
+        public String Name { get; set; }
+
+        public String Country { get; set; }
+
+        public String State { get; set; }
+
+    }
+}
---
+++ MassProvisioning/Model/UserMassProvisioning.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MassProvisioning.Model
+{
+    public class UserMassProvisioning
+    {
+        public String Id { get; set; }
+
+        public String FirstName { get; set; }
+
+        public String LastName { get; set; }
+
+        public String Email { get; set; }
+
+        public String Password { get; set; }
+    }
+}
