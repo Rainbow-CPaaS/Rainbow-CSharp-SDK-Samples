05344339c894534652817d71d2a06a8cd3440095
Christophe Irles <christophe.irles@al-enterprise.com>
2021-08-06 09:48:15 +0200

    [MultiPlatform] EditorExpandableWithMaxLines: Add bindable properties ValidationCommand and ValidationKeyModifier. Use them in MessageInput

--- MultiPlatformApplication/MultiPlatformApplication/Controls/EditorExpandableWithMaxLines.xaml.cs
+++ MultiPlatformApplication/MultiPlatformApplication/Controls/EditorExpandableWithMaxLines.xaml.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Windows.Input;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -13,8 +13,81 @@ namespace MultiPlatformApplication.Controls
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class EditorExpandableWithMaxLines : ContentView
     {
-
         public event EventHandler<TextChangedEventArgs> TextChanged;
+
+#region ValidationCommand Property
+
+        public static readonly BindableProperty ValidationCommandProperty =
+            BindableProperty.Create(nameof(ValidationCommand),
+            typeof(ICommand),
+            typeof(EditorExpandableWithMaxLines),
+            defaultValue: null,
+            defaultBindingMode: BindingMode.OneWay,
+            propertyChanged: ValidationCommandPropertyProperyChanged);
+
+        private static void ValidationCommandPropertyProperyChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            if( (bindable != null) && (newValue != null) )
+            {
+                EditorExpandableWithMaxLines editorExpandableWithMaxLines = (EditorExpandableWithMaxLines)bindable;
+                MultiPlatformApplication.Effects.Entry.SetValidationCommand(editorExpandableWithMaxLines.Editor, (ICommand)newValue);
+            }
+        }
+
+        public ICommand ValidationCommand
+        {
+            get
+            {
+                var obj = base.GetValue(ValidationCommandProperty);
+                if (obj is ICommand)
+                    return (ICommand)obj;
+                return null;
+            }
+            set
+            {
+                base.SetValue(ValidationCommandProperty, value);
+            }
+        }
+
+#endregion ValidationCommandProperty Property
+
+
+#region ValidationKeyModifier Property
+
+        public static readonly BindableProperty ValidationKeyModifierProperty =
+            BindableProperty.Create(nameof(ValidationKeyModifier),
+            typeof(String),
+            typeof(EditorExpandableWithMaxLines),
+            defaultValue: "",
+            defaultBindingMode: BindingMode.OneWay,
+            propertyChanged: ValidationKeyModifierProperyChanged);
+
+        private static void ValidationKeyModifierProperyChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            if( (bindable != null) && (newValue != null) )
+            {
+                EditorExpandableWithMaxLines editorExpandableWithMaxLines = (EditorExpandableWithMaxLines)bindable;
+                MultiPlatformApplication.Effects.Entry.SetValidationKeyModifier(editorExpandableWithMaxLines.Editor, (string)newValue);
+            }
+        }
+
+        public String ValidationKeyModifier
+        {
+            get
+            {
+                var obj = base.GetValue(ValidationKeyModifierProperty);
+                if (obj is String)
+                    return (String)obj;
+                return null;
+            }
+            set
+            {
+                base.SetValue(ValidationKeyModifierProperty, value);
+            }
+        }
+
+#endregion ValidationKeyModifier Property
+
 
 #region Text Property
 
@@ -45,6 +118,7 @@ namespace MultiPlatformApplication.Controls
                 base.SetValue(TextProperty, value);
             }
         }
+
 #endregion Text Property
 
 #region Placeholder Property
@@ -76,6 +150,7 @@ namespace MultiPlatformApplication.Controls
                 base.SetValue(PlaceholderProperty, value);
             }
         }
+
 #endregion Placeholder Property
 
 
@@ -108,6 +183,7 @@ namespace MultiPlatformApplication.Controls
                 base.SetValue(MaxLinesProperty, value);
             }
         }
+
 #endregion MaxLines Property
 
         double heigthOneLine = 0;
@@ -183,7 +259,7 @@ namespace MultiPlatformApplication.Controls
                     if( (heigthOneLine != 0) && (heigthTwoLines != 0) )
                         ScrollView.HeightRequest = heigthOneLine + (heigthTwoLines - heigthOneLine) * (MaxLines - 1);
                     else
-                        ScrollView.HeightRequest = 80; // Need a backup ... 
+                        ScrollView.HeightRequest = 80; // Need a backup value ... 
                 }
             }
             else
