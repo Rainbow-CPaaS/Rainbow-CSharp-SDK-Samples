b77b1041a0c21858017d9f044c690de4c2bf34c1
Christophe Irles <christophe.irles@al-enterprise.com>
2021-08-18 10:52:16 +0200

    [MultiPlatform] Add BtnAction display in MessageContent (where mouse is used)

--- MultiPlatformApplication/MultiPlatformApplication/Controls/MessageContent.xaml.cs
+++ MultiPlatformApplication/MultiPlatformApplication/Controls/MessageContent.xaml.cs
@@ -1,4 +1,5 @@
-﻿using MultiPlatformApplication.Helpers;
+﻿using MultiPlatformApplication.Effects;
+using MultiPlatformApplication.Helpers;
 using MultiPlatformApplication.Models;
 using Rainbow.Model;
 using System;
@@ -19,37 +20,95 @@ namespace MultiPlatformApplication.Controls
 
         private MessageElementModel message = null;
 
-        MessageContentUrgency messageContentUrgency = null;
-        MessageContentReply messageContentReply = null;
-        MessageContentBody messageContentBody = null;
-        MessageContentBodyWithImg messageContentBodyWithImg = null;
-        MessageContentBodyWithCode messageContentBodyWithCode = null;
-        MessageContentAttachment messageContentAttachment = null;
-        MessageContentDeleted messageContentDeleted = null;
-        MessageContentForward messageContentForward = null;
+        private MessageContentUrgency messageContentUrgency = null;
+        private MessageContentReply messageContentReply = null;
+        private MessageContentBody messageContentBody = null;
+        private MessageContentBodyWithImg messageContentBodyWithImg = null;
+        private MessageContentBodyWithCode messageContentBodyWithCode = null;
+        private MessageContentAttachment messageContentAttachment = null;
+        private MessageContentDeleted messageContentDeleted = null;
+        private MessageContentForward messageContentForward = null;
+
+        // Define commands used for  Mouse Over / Mouse Out purpose
+        public MouseOverAndOutModel mouseCommands { get; set; }
+        private Boolean needActionButton = false;
 
         public MessageContent()
         {
             InitializeComponent();
 
+            // Add Mouse out/oVer management only for some platforms
+            if ((Device.RuntimePlatform == Device.UWP)
+                || (Device.RuntimePlatform == Device.WPF)
+                || (Device.RuntimePlatform == Device.macOS))
+            {
+                needActionButton = true;
+
+                mouseCommands = new MouseOverAndOutModel();
+                mouseCommands.MouseOverCommand = new RelayCommand<object>(new Action<object>(MouseOverCommand));
+                mouseCommands.MouseOutCommand = new RelayCommand<object>(new Action<object>(MouseOutCommand));
+
+                //Add mouse over effect
+                MouseOverEffect.SetCommand(RootGrid, mouseCommands.MouseOverCommand);
+
+                //Add mouse out effect
+                MouseOutEffect.SetCommand(RootGrid, mouseCommands.MouseOutCommand);
+
+                BtnAction.Command = new RelayCommand<object>(new Action<object>(BtnActionCommand));
+            }
+
             BowViewForMinimalWidth.WidthRequest = MINIMAL_MESSAGE_WIDTH;
         }
 
+        private void BtnActionCommand(object obj)
+        {
+            // TODO
+        }
+
+        private void MouseOverCommand(object obj)
+        {
+            if (needActionButton)
+            {
+                BtnAction.IsVisible = true;
+            }
+        }
+
+        private void MouseOutCommand(object obj)
+        {
+            if (needActionButton)
+            {
+                BtnAction.IsVisible = false;
+            }
+        }
 
         protected override void OnBindingContextChanged()
         {
-            if( (BindingContext != null) && (message == null) )
+            if ((BindingContext != null) && (message == null))
             {
                 message = (MessageElementModel)BindingContext;
-                if(message != null)
+                if (message != null)
                 {
-                    String backgroundColorKey;
-                    if (message.Peer.Id == Helper.SdkWrapper.GetCurrentContactId())
-                        backgroundColorKey = "ColorConversationStreamMessageCurrentUserBackGround";
-                    else
-                        backgroundColorKey = "ColorConversationStreamMessageOtherUserBackGround";
-                    Frame.BackgroundColor = Helper.GetResourceDictionaryById<Color>(backgroundColorKey);
+                    Color color;
+                    Color backgroundColor;
 
+                    if (message.Peer.Id == Helper.SdkWrapper.GetCurrentContactId())
+                    {
+                        color = Helper.GetResourceDictionaryById<Color>("ColorConversationStreamMessageCurrentUserFont");
+                        backgroundColor = Helper.GetResourceDictionaryById<Color>("ColorConversationStreamMessageCurrentUserBackGround");
+                    }
+                    else
+                    {
+                        color = Helper.GetResourceDictionaryById<Color>("ColorConversationStreamMessageOtherUserFont");
+                        backgroundColor = Helper.GetResourceDictionaryById<Color>("ColorConversationStreamMessageOtherUserBackGround");
+                    }
+
+                    Frame.BackgroundColor = backgroundColor;
+                    if (needActionButton)
+                    {
+                        BtnAction.ImageSourceId = "Font_EllipsisV|" + color.ToHex();
+                        BtnAction.BackgroundColor = backgroundColor;
+                        BtnAction.BackgroundColorOnMouseOver = ColorInterpolator.InterpolateBetween(color, backgroundColor, 0.5);
+                    }
 
                     // Add Urgency element ?
                     if (message.Content.Urgency != UrgencyType.Std)
@@ -58,18 +117,22 @@ namespace MultiPlatformApplication.Controls
                         {
                             messageContentUrgency = new MessageContentUrgency();
                             messageContentUrgency.BindingContext = message;
-                            MainGrid.Children.Add(messageContentUrgency, 0, 0);
+                            HeaderGrid.Children.Add(messageContentUrgency, 0, 0);
+
+                            // Need to define here the BackgroundColor of the HeaderGrid to ensure to have correct display if the message body is short
+                            Helper.GetUrgencyInfo(message.Content.Urgency.ToString(), out backgroundColor, out color, out String title, out String label, out String imageSourceId);
+                            HeaderGrid.BackgroundColor = backgroundColor;
                         }
                     }
 
                     // Add forward header
-                    if(message.IsForwarded)
+                    if (message.IsForwarded)
                     {
-                        if(messageContentForward == null)
+                        if (messageContentForward == null)
                         {
                             messageContentForward = new MessageContentForward();
                             messageContentForward.BindingContext = message;
-                            MainGrid.Children.Add(messageContentForward, 0, 0);
+                            HeaderGrid.Children.Add(messageContentForward, 0, 0);
                         }
                     }
 
@@ -80,20 +143,20 @@ namespace MultiPlatformApplication.Controls
                         {
                             messageContentReply = new MessageContentReply();
                             messageContentReply.BindingContext = message;
-                            MainGrid.Children.Add(messageContentReply, 0, 1);
+                            HeaderGrid.Children.Add(messageContentReply, 0, 1);
                         }
                     }
 
                     // Add Body element ?
                     if (!String.IsNullOrEmpty(message.Content?.Body))
                     {
-                        if(message.Content.Body.StartsWith("/img", StringComparison.InvariantCultureIgnoreCase) )
+                        if (message.Content.Body.StartsWith("/img", StringComparison.InvariantCultureIgnoreCase))
                         {
                             if (messageContentBodyWithImg == null)
                             {
                                 messageContentBodyWithImg = new MessageContentBodyWithImg();
                                 messageContentBodyWithImg.BindingContext = message;
-                                MainGrid.Children.Add(messageContentBodyWithImg, 0, 2);
+                                ContentGrid.Children.Add(messageContentBodyWithImg, 0, 0);
                             }
                         }
                         else if (message.Content.Body.StartsWith("/code", StringComparison.InvariantCultureIgnoreCase))
@@ -102,22 +165,25 @@ namespace MultiPlatformApplication.Controls
                             {
                                 messageContentBodyWithCode = new MessageContentBodyWithCode();
                                 messageContentBodyWithCode.BindingContext = message;
-                                MainGrid.Children.Add(messageContentBodyWithCode, 0, 2);
+                                ContentGrid.Children.Add(messageContentBodyWithCode, 0, 0);
                             }
                         }
                         else if (messageContentBody == null)
                         {
                             messageContentBody = new MessageContentBody();
                             messageContentBody.BindingContext = message;
-                            MainGrid.Children.Add(messageContentBody, 0, 2);
+                            ContentGrid.Children.Add(messageContentBody, 0, 0);
                         }
                     }
-                    else if(message.Content.Type == "deletedMessage")
+                    else if (message.Content.Type == "deletedMessage")
                     {
+                        // We don't need action button in this case
+                        needActionButton = false;
+
                         // Create MessageContentDeleted
                         messageContentDeleted = new MessageContentDeleted();
                         messageContentDeleted.BindingContext = message;
-                        MainGrid.Children.Add(messageContentDeleted, 0, 2);
+                        ContentGrid.Children.Add(messageContentDeleted, 0, 0);
                     }
 
                     // Add Attachment element ?
@@ -127,7 +193,7 @@ namespace MultiPlatformApplication.Controls
                         {
                             messageContentAttachment = new MessageContentAttachment();
                             messageContentAttachment.BindingContext = message;
-                            MainGrid.Children.Add(messageContentAttachment, 0, 3);
+                            ContentGrid.Children.Add(messageContentAttachment, 0, 1);
                         }
                     }
                 }
