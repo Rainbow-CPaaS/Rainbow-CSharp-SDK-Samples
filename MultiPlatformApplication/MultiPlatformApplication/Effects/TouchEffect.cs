d72f7d3354dc1f1462a6cd520ef8ec30fcdda51a
Christophe Irles <christophe.irles@al-enterprise.com>
2021-08-19 13:26:21 +0200

    [MultiPlatform] Remove LongPress effect and replace it by TouchEffect More granularity: Entered, Pressed, Moved, Released, Exited, Cancelled And permit to get position

---
+++ MultiPlatformApplication/MultiPlatformApplication/Effects/TouchEffect.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace MultiPlatformApplication.Effects
+{
+    public class TouchEffect : RoutingEffect
+    {
+        // Based on: https://github.com/xamarin/xamarin-forms-samples/tree/main/Effects/TouchTrackingEffect
+
+        public event EventHandler<TouchActionEventArgs> TouchAction;
+
+        public TouchEffect() : base("MultiPlatformApplication.CrossEffects.TouchEffect")
+        {
+        }
+
+        public bool Capture { set; get; }
+
+        public void OnTouchAction(Element element, TouchActionEventArgs args)
+        {
+            TouchAction?.Invoke(element, args);
+        }
+    }
+
+    public enum TouchActionType
+    {
+        Entered,
+        Pressed,
+        Moved,
+        Released,
+        Exited,
+        Cancelled
+    }
+}
