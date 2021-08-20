9d8f3f7f2c0f7877fc4e404f322e2fb36042602e
Christophe Irles <christophe.irles@al-enterprise.com>
2021-08-20 15:02:59 +0200

    [MultiPlatform] Add RectEventArgs class

---
+++ MultiPlatformApplication/MultiPlatformApplication/Events/RectEventArgs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace MultiPlatformApplication.Events
+{
+    public class RectEventArgs : EventArgs
+    {
+        public RectEventArgs(Rect Rect)
+        {
+            this.Rect = Rect;
+        }
+
+        public Rect Rect { private set; get; }
+    }
+}
