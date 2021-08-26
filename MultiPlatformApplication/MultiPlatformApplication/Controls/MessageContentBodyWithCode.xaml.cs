1092c792624c661ade855de2d40bcb07097dcf95
Christophe Irles <christophe.irles@al-enterprise.com>
2021-08-26 10:48:57 +0200

    [MultiPlatform] MessageContentBodyWithCode: selectable text is possible only on "desktop" version

--- MultiPlatformApplication/MultiPlatformApplication/Controls/MessageContentBodyWithCode.xaml.cs
+++ MultiPlatformApplication/MultiPlatformApplication/Controls/MessageContentBodyWithCode.xaml.cs
@@ -1,4 +1,6 @@
-﻿using MultiPlatformApplication.Models;
+﻿using MultiPlatformApplication.Effects;
+using MultiPlatformApplication.Helpers;
+using MultiPlatformApplication.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,6 +20,10 @@ namespace MultiPlatformApplication.Controls
         public MessageContentBodyWithCode()
         {
             InitializeComponent();
+
+            // On Desktop platform, Label can be selected
+            if (Helper.IsDesktopPlatform())
+                SelectableLabelEffect.SetEnabled(Label, true);
 
             this.BindingContextChanged += MessageContentBodyWithCode_BindingContextChanged;
         }
