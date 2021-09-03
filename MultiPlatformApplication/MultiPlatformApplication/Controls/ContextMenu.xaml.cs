b736cb023b897017b66392bee72dc730f7430625
Christophe Irles <christophe.irles@al-enterprise.com>
2021-09-03 15:34:47 +0200

    [MultiPlatform] ContextMenu UI Component with 3 DataTemplate to easily add ContextMenu with/without Icon and with/without Description

---
+++ MultiPlatformApplication/MultiPlatformApplication/Controls/ContextMenu.xaml.cs
@@ -0,0 +1,175 @@
+﻿using MultiPlatformApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MultiPlatformApplication.Controls
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ContextMenu : ContentView
+    {
+        ContextMenuModel originalContextMenuModel = null;
+        ContextMenuModel contextMenuModelUsed = new ContextMenuModel();
+
+        Boolean iconUsed = false;
+        Boolean descriptionUsed = false;
+
+#region CommandProperty
+
+        public static readonly BindableProperty CommandProperty =
+            BindableProperty.Create(nameof(Command), typeof(ICommand), typeof(CustomButton), null);
+
+        public ICommand Command
+        {
+            get { return (ICommand)GetValue(CommandProperty); }
+            set { SetValue(CommandProperty, value); }
+        }
+
+#endregion CommandProperty
+
+        public ContextMenu()
+        {
+            InitializeComponent();
+
+            ListView.ItemSelected += ListView_ItemSelected;
+        }
+
+        ~ContextMenu()
+        {
+            if (originalContextMenuModel != null)
+            {
+                originalContextMenuModel.ItemVisibilityChanged -= OriginalContextMenuModel_ItemVisibilityChanged;
+                if (originalContextMenuModel.Items != null)
+                    originalContextMenuModel.Items.CollectionChanged -= OriginalContextMenuModel_Items_CollectionChanged;
+            }
+
+            ListView.ItemSelected -= ListView_ItemSelected;
+
+            ListView.BindingContext = null;
+            ListView.ItemTemplate = null;
+
+            contextMenuModelUsed = null;
+            originalContextMenuModel = null;
+        }
+
+        protected override void OnBindingContextChanged()
+        {
+            if(BindingContext is ContextMenuModel contextMenuModel)
+            {
+                // If ContextMenuModel has changed we need to clear some stuff
+                if(originalContextMenuModel != contextMenuModel)
+                {
+                    if (originalContextMenuModel != null)
+                    {
+                        originalContextMenuModel.ItemVisibilityChanged -= OriginalContextMenuModel_ItemVisibilityChanged;
+                        if(originalContextMenuModel.Items != null)
+                            originalContextMenuModel.Items.CollectionChanged -= OriginalContextMenuModel_Items_CollectionChanged;
+                    }
+                }
+
+                // Store new ContextMenuModel
+                originalContextMenuModel = contextMenuModel;
+
+                // We need to handle collection modification
+                originalContextMenuModel.ItemVisibilityChanged += OriginalContextMenuModel_ItemVisibilityChanged;
+                originalContextMenuModel.Items.CollectionChanged += OriginalContextMenuModel_Items_CollectionChanged;
+
+                // Create ContextMenuModel used for display
+                UpdateContextMenuModelUsedForUI();
+            }
+        }
+
+        private void UpdateContextMenuModelUsedForUI()
+        {
+            // Set default values
+            iconUsed = false;
+            descriptionUsed = false;
+
+            // Clear ContextMenuModel used
+            contextMenuModelUsed.Clear();
+
+            // Loop on each item
+            foreach (var item in originalContextMenuModel.Items)
+            {
+                if (!String.IsNullOrEmpty(item.ImageSourceId))
+                    iconUsed = true;
+
+                if (!String.IsNullOrEmpty(item.Description))
+                    descriptionUsed = true;
+
+                if (item.IsVisible)
+                    contextMenuModelUsed.Add(item);
+            }
+
+            // Set correct DataTemplate
+            if (descriptionUsed)
+            {
+                // TODO
+                ListView.ItemTemplate = ContextMenuWithDescriptionDataTemplate;
+            }
+            else
+            {
+                if (iconUsed)
+                    ListView.ItemTemplate = ContextMenuWithIconDataTemplate;
+                else
+                    ListView.ItemTemplate = ContextMenuWithoutIconDataTemplate;
+            }
+
+            // Set the binding context
+            ListView.BindingContext = contextMenuModelUsed;
+
+            // Update Heigth
+            SetHeigthAccordingModel();
+        }
+
+        private void OriginalContextMenuModel_ItemVisibilityChanged(object sender, EventArgs e)
+        {
+            UpdateContextMenuModelUsedForUI();
+        }
+
+        private void SetHeigthAccordingModel()
+        {
+            if (contextMenuModelUsed?.Items?.Count > 0)
+            {
+                if (descriptionUsed)
+                {
+                    ListView.RowHeight = 54;
+                    ListView.HeightRequest = (54 * contextMenuModelUsed.Items.Count);
+                }
+                else
+                {
+                    ListView.RowHeight = 28;
+                    ListView.HeightRequest = (28 * contextMenuModelUsed.Items.Count);
+                }
+            }
+            else
+                ListView.HeightRequest = 0;
+        }
+
+        private void OriginalContextMenuModel_Items_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+            SetHeigthAccordingModel();
+        }
+
+        private void ListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItemIndex != -1)
+            {
+                String param = null;
+                if(contextMenuModelUsed?.Items.Count > e.SelectedItemIndex)
+                    param = contextMenuModelUsed.Items[e.SelectedItemIndex].Id;
+
+                if (Command != null && Command.CanExecute(param))
+                    Command.Execute(param);
+
+                // Reset selection
+                ListView.SelectedItem = null;
+            }
+        }
+    }
+}
--- MultiPlatformApplication/MultiPlatformApplication/Views/TestPage3.xaml.cs
+++ MultiPlatformApplication/MultiPlatformApplication/Views/TestPage3.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using MultiPlatformApplication.Controls;
 using MultiPlatformApplication.Effects;
 using MultiPlatformApplication.Helpers;
+using MultiPlatformApplication.Models;
+using Rainbow.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,11 +17,12 @@ namespace MultiPlatformApplication.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class TestPage3 : CtrlContentPage
     {
-        TapGestureRecognizer tapGestureRecognizer;
-
         int counter = 0;
-
+        int dynamicContextMenuCounter = 0;
         Boolean newPopupAdded = false;
+
+        ContextMenuModel contextMenuModel;
+        View dynamicContextMenu = null;
 
         public TestPage3()
         {
@@ -33,17 +36,14 @@ namespace MultiPlatformApplication.Views
 
             AddPopupButton.Command = new RelayCommand<object>(new Action<object>(AddPopupButtonCommand));
             DisplayPopupButton.Command = new RelayCommand<object>(new Action<object>(DisplayPopupButtonCommand));
+            UpdatePopupModelButton.Command = new RelayCommand<object>(new Action<object>(UpdatePopupModelButtonCommand));
 
             Logo.Command = new RelayCommand<object>(new Action<object>(LogoCommand));
-
-            //tapGestureRecognizer = new TapGestureRecognizer();
-            //tapGestureRecognizer.Command = new RelayCommand<object>(new Action<object>(TapGestureRecognizerCommand));
-            //Logo.GestureRecognizers.Add(tapGestureRecognizer);
         }
 
         private void LogoCommand(object obj)
         {
-            //Popup.Show("ContextMenu1", LayoutAlignment.Center, LayoutAlignment.Start, new Point(0,10));
+            Popup.Show("ContextMenu1", null, LayoutAlignment.Center, false,  LayoutAlignment.Start, false, new Point(0,10));
         }
 
         private void CustomButton1Command(object obj)
@@ -57,37 +57,11 @@ namespace MultiPlatformApplication.Views
             else
                 Popup.Show("ContextMenu1", "CustomButton1", LayoutAlignment.Start, false, LayoutAlignment.Start, true, new Point(), -1);
 
-            //if (counter % 4 == 0)
-            //    Popup.Show("ContextMenu1", "CustomButton1", LayoutAlignment.Start, true, LayoutAlignment.End, true, new Point(), -1);
-            //else if (counter % 4 == 1)
-            //    Popup.Show("ContextMenu1", "CustomButton1", LayoutAlignment.End, true, LayoutAlignment.End, true, new Point(), -1);
-            //else if (counter % 4 == 2)
-            //    Popup.Show("ContextMenu1", "CustomButton1", LayoutAlignment.End, true, LayoutAlignment.Start, true, new Point(), -1);
-            //else
-            //    Popup.Show("ContextMenu1", "CustomButton1", LayoutAlignment.Start, true, LayoutAlignment.Start, true, new Point(), -1);
-
-            //if (counter % 4 == 0)
-            //    Popup.Show("ContextMenu1", "CustomButton1", LayoutAlignment.Start, false, LayoutAlignment.End, false, new Point(), -1);
-            //else if (counter % 4 == 1)
-            //    Popup.Show("ContextMenu1", "CustomButton1", LayoutAlignment.End, false, LayoutAlignment.End, false, new Point(), -1);
-            //else if (counter % 4 == 2)
-            //    Popup.Show("ContextMenu1", "CustomButton1", LayoutAlignment.End, false, LayoutAlignment.Start, false, new Point(), -1);
-            //else
-            //    Popup.Show("ContextMenu1", "CustomButton1", LayoutAlignment.Start, false, LayoutAlignment.Start, false, new Point(), -1);
-
-            //Popup.Show("ContextMenu1", "CustomButton1");
-
             counter++;
         }
 
         private void CustomButton2Command(object obj)
         {
-            //if (counter % 2 == 0)
-            //    Popup.ShowContextMenu("ContextMenu2", "CustomButton2");
-            //else
-            //    Popup.ShowContextMenu("ContextMenu2", "CustomButton2");
-
-
             if (counter % 4 == 0)
                 Popup.Show("ContextMenu2", "CustomButton2", LayoutAlignment.Start, false, LayoutAlignment.End, false, new Point(), -1);
             else if (counter % 4 == 1)
@@ -96,19 +70,6 @@ namespace MultiPlatformApplication.Views
                 Popup.Show("ContextMenu2", "CustomButton2", LayoutAlignment.End, false, LayoutAlignment.Start, false, new Point(), -1);
             else
                 Popup.Show("ContextMenu2", "CustomButton2", LayoutAlignment.Start, false, LayoutAlignment.Start, false, new Point(), -1);
-
-
-            //if (counter % 4 == 0)
-            //    Popup.Show("ContextMenu2", "MainGridTestPage3", LayoutAlignment.Start, false, LayoutAlignment.End, false, new Point(), -1);
-            //else if (counter % 4 == 1)
-            //    Popup.Show("ContextMenu2", "MainGridTestPage3", LayoutAlignment.End, false, LayoutAlignment.End, false, new Point(), -1);
-            //else if (counter % 4 == 2)
-            //    Popup.Show("ContextMenu2", "MainGridTestPage3", LayoutAlignment.End, false, LayoutAlignment.Start, false, new Point(), -1);
-            //else
-            //    Popup.Show("ContextMenu2", "MainGridTestPage3", LayoutAlignment.Start, false, LayoutAlignment.Start, false, new Point(), -1);
-
-
-            //Popup.Show("ContextMenu2", "CustomButton2");
 
             counter++;
         }
@@ -136,30 +97,126 @@ namespace MultiPlatformApplication.Views
         {
             if (!newPopupAdded)
             {
-                Frame frameTest = new Frame { AutomationId = "FrameTest", Margin = 0, Padding = 0, BackgroundColor = Color.Red, WidthRequest = 50, HeightRequest = 50 };
-                Popup.Add(this, frameTest, PopupType.ContextMenu);
+                if (dynamicContextMenuCounter % 3 == 2)
+                {
+                    contextMenuModel = new ContextMenuModel();
+                    contextMenuModel.Add(new ContextMenuItemModel() { Id = "firstname", Title = Helper.SdkWrapper.GetLabel("firstnameOrder") });
+                    contextMenuModel.Add(new ContextMenuItemModel() { Id = "lastname", Title = Helper.SdkWrapper.GetLabel("lastnameOrder") });
+                    contextMenuModel.Add(new ContextMenuItemModel() { Id = "company", Title = Helper.SdkWrapper.GetLabel("companyOrder") });
+
+                    dynamicContextMenu = new Controls.ContextMenu { AutomationId = "DynamicContextMenu", WidthRequest = 140 };
+                    dynamicContextMenu.BindingContext = contextMenuModel;
+                }
+                else if (dynamicContextMenuCounter % 3 == 1)
+                {
+                    Color color;
+                    String colorHex;
+                    String imageSourceId;
+
+                    contextMenuModel = new ContextMenuModel();
+
+                    color = Helper.GetResourceDictionaryById<Color>("ColorConversationStreamMessageOtherUserFont");
+                    colorHex = color.ToHex();
+
+                    imageSourceId = "Font_PencilAlt|" + colorHex;
+                    contextMenuModel.Add(new ContextMenuItemModel() { Id = "edit", ImageSourceId = imageSourceId, Title = Helper.SdkWrapper.GetLabel("edit") });
+
+                    imageSourceId = "Font_FileDownload|" + colorHex;
+                    contextMenuModel.Add(new ContextMenuItemModel() { Id = "download", ImageSourceId = imageSourceId, Title = Helper.SdkWrapper.GetLabel("download") });
+
+                    imageSourceId = "Font_Reply|" + colorHex;
+                    contextMenuModel.Add(new ContextMenuItemModel() { Id = "reply", ImageSourceId = imageSourceId, Title = Helper.SdkWrapper.GetLabel("replyToMessage") });
+
+                    imageSourceId = "Font_ArrowRight|" + colorHex;
+                    contextMenuModel.Add(new ContextMenuItemModel() { Id = "forward", ImageSourceId = imageSourceId, Title = Helper.SdkWrapper.GetLabel("forwardMessage") });
+
+                    imageSourceId = "Font_Copy|" + colorHex;
+                    contextMenuModel.Add(new ContextMenuItemModel() { Id = "copy", ImageSourceId = imageSourceId, Title = Helper.SdkWrapper.GetLabel("copy") });
+
+                    imageSourceId = "Font_TrashAlt|" + colorHex;
+                    contextMenuModel.Add(new ContextMenuItemModel() { Id = "delete", ImageSourceId = imageSourceId, Title = Helper.SdkWrapper.GetLabel("delete") });
+
+                    imageSourceId = "Font_CloudDownloadAlt|" + colorHex;
+                    contextMenuModel.Add(new ContextMenuItemModel() { Id = "save", ImageSourceId = imageSourceId, Title = Helper.SdkWrapper.GetLabel("save") });
+
+                    dynamicContextMenu = new Controls.ContextMenu { AutomationId = "DynamicContextMenu", WidthRequest = 180 };
+                    dynamicContextMenu.BindingContext = contextMenuModel;
+                }
+                else if (dynamicContextMenuCounter % 3 == 0)
+                {
+                    Color color;
+                    Color backgroundColor;
+                    String title;
+                    String label;
+                    String imageSourceId;
+
+                    ContextMenuItemModel messageUrgencyModelItem;
+
+                    String urgencyType;
+
+                    contextMenuModel = new ContextMenuModel();
+
+                    urgencyType = UrgencyType.High.ToString();
+                    Helper.GetUrgencyInfo(urgencyType, out backgroundColor, out color, out title, out label, out imageSourceId);
+                    messageUrgencyModelItem = new ContextMenuItemModel() { Id = urgencyType, ImageSourceId = imageSourceId, Title = title, Description = label, TextColor = color };
+                    contextMenuModel.Add(messageUrgencyModelItem);
+
+                    urgencyType = UrgencyType.Middle.ToString();
+                    Helper.GetUrgencyInfo(urgencyType, out backgroundColor, out color, out title, out label, out imageSourceId);
+                    messageUrgencyModelItem = new ContextMenuItemModel() { Id = urgencyType, ImageSourceId = imageSourceId, Title = title, Description = label, TextColor = color };
+                    contextMenuModel.Add(messageUrgencyModelItem);
+
+                    urgencyType = UrgencyType.Low.ToString();
+                    Helper.GetUrgencyInfo(urgencyType, out backgroundColor, out color, out title, out label, out imageSourceId);
+                    messageUrgencyModelItem = new ContextMenuItemModel() { Id = urgencyType, ImageSourceId = imageSourceId, Title = title, Description = label, TextColor = color };
+                    contextMenuModel.Add(messageUrgencyModelItem);
+
+                    urgencyType = UrgencyType.Std.ToString();
+                    Helper.GetUrgencyInfo(urgencyType, out backgroundColor, out color, out title, out label, out imageSourceId);
+                    messageUrgencyModelItem = new ContextMenuItemModel() { Id = urgencyType, ImageSourceId = imageSourceId, Title = title, Description = label, TextColor = color };
+                    contextMenuModel.Add(messageUrgencyModelItem);
+
+                    dynamicContextMenu = new Controls.ContextMenu { AutomationId = "DynamicContextMenu", WidthRequest = 280 };
+                    dynamicContextMenu.BindingContext = contextMenuModel;
+                }
+
+                // Increase counter
+                dynamicContextMenuCounter++;
+
+                if (dynamicContextMenu != null)
+                {
+                    Popup.Add(this, dynamicContextMenu, PopupType.ContextMenu);
+                    newPopupAdded = !newPopupAdded;
+                }
             }
             else
             {
-                Popup.Remove(this, "FrameTest");
+                Popup.Remove(this, "DynamicContextMenu");
+                newPopupAdded = !newPopupAdded;
+
+                dynamicContextMenu = null;
             }
-            newPopupAdded = !newPopupAdded;
 
             UpdateLabel();
             DisplayPopupButton.IsEnabled = newPopupAdded;
-
+            UpdatePopupModelButton.IsEnabled = newPopupAdded;
         }
 
         private void DisplayPopupButtonCommand(object obj)
         {
             if (newPopupAdded)
             {
-                Popup.Show("FrameTest", "DisplayPopupButton");
+                Popup.Show("DynamicContextMenu", "DisplayPopupButton");
             }
         }
 
+        private void UpdatePopupModelButtonCommand(object obj)
+        {
+            if(contextMenuModel?.Items?.Count >= 2)
+            {
+                contextMenuModel.Items[1].IsVisible = false;
+            }
+        }
         
-
-
     }
 }
