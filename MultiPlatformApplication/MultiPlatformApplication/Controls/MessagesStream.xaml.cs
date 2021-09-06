23e27a93432aa11383c65c2ce0791adcdeb51364
Christophe Irles <christophe.irles@al-enterprise.com>
2021-09-06 11:53:06 +0200

    [MultiPlatform] MessagesStream: Remove usage of RelativeLayout

--- MultiPlatformApplication/MultiPlatformApplication/Controls/MessagesStream.xaml.cs
+++ MultiPlatformApplication/MultiPlatformApplication/Controls/MessagesStream.xaml.cs
@@ -14,54 +14,26 @@ namespace MultiPlatformApplication.Controls
 	public partial class MessagesStream : ContentView
 	{
         String conversationId;
+
         MessagesStreamViewModel vm;
 
         public MessagesStream ()
 		{
 			InitializeComponent ();
 
-            // Define MessageInput Constraints
-            RelativeLayout.SetYConstraint(MessageInput, Constraint.RelativeToParent((rl) =>
-            {
-                if ((rl.Height != -1) && (MessageInput.Height != -1))
-                {
-                    return rl.Height - MessageInput.Height;
-                }
-                return -1;
-            }));
-
-            RelativeLayout.SetWidthConstraint(MessageInput, Constraint.RelativeToParent((rl) =>
-            {
-                return rl.Width;
-            }));
-
-
-            // Define ContentViewPlatformSpecific Constraints
-            RelativeLayout.SetWidthConstraint(ContentViewPlatformSpecific, Constraint.RelativeToParent((rl) =>
-            {
-                return rl.Width;
-            }));
-
-            RelativeLayout.SetHeightConstraint(ContentViewPlatformSpecific, Constraint.RelativeToParent((rl) =>
-            {
-                if ((rl.Height != -1) && (MessageInput.Height != -1))
-                {
-                    return rl.Height - MessageInput.Height;
-                }
-                return -1;
-            }));
-
             vm = new MessagesStreamViewModel(RootElement);
-            this.BindingContextChanged += MessagesStream_BindingContextChanged;
-		}
 
+            this.BindingContextChanged += MessagesStream_BindingContextChanged;
+
+            
+		}
 
         private void MessagesStream_BindingContextChanged(object sender, EventArgs e)
         {
-            if(BindingContext != null)
+            if (BindingContext != null)
             {
                 String id = (String)BindingContext;
-                if( (!String.IsNullOrEmpty(id)) && (id != conversationId) )
+                if ((!String.IsNullOrEmpty(id)) && (id != conversationId))
                 {
                     conversationId = id;
                     vm.Initialize(conversationId);
