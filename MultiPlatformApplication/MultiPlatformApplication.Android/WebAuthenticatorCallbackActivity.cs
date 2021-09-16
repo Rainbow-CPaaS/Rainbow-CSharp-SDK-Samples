15a2652e07c190f6e4f5facb27b16cc86f5f20e5
Christophe Irles <christophe.irles@al-enterprise.com>
2021-09-16 16:10:50 +0200

    [MultiPlatform] Android: Add "rainbow" as intent for SSO purpose

---
+++ MultiPlatformApplication/MultiPlatformApplication.Android/WebAuthenticatorCallbackActivity.cs
@@ -0,0 +1,10 @@
+﻿using Android.App;
+using Android.Content.PM;
+
+[Activity(NoHistory = true, LaunchMode = LaunchMode.SingleTop)]
+[IntentFilter(new[] { Android.Content.Intent.ActionView },
+    Categories = new[] { Android.Content.Intent.CategoryDefault, Android.Content.Intent.CategoryBrowsable },
+    DataScheme = "rainbow")]
+public class WebAuthenticationCallbackActivity : Xamarin.Essentials.WebAuthenticatorCallbackActivity
+{
+}
