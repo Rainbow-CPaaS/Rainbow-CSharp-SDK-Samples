1c76cb4e60f6ceea665ad948184cddf3fe8d9080
Christophe Irles <christophe.irles@al-enterprise.com>
2021-10-21 12:06:15 +0200

    [MultiPlatform] MainActivity: override several methods OnStart(), OnResume(), OnStop(), OnRestart(), OnPause(), OnDestroy()

--- MultiPlatformApplication/MultiPlatformApplication.Android/MainActivity.cs
+++ MultiPlatformApplication/MultiPlatformApplication.Android/MainActivity.cs
@@ -10,6 +10,13 @@ namespace MultiPlatformApplication.Droid
     [Activity(Label = "MultiPlatformApplication", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -18,11 +25,54 @@ namespace MultiPlatformApplication.Droid
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
         }
-        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
-        {
-            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
-            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        protected override void OnSaveInstanceState(Bundle outState)
+        {
+            base.OnSaveInstanceState(outState);
         }
+
+        protected override void OnRestoreInstanceState(Bundle savedInstanceState)
+        {
+            base.OnRestoreInstanceState(savedInstanceState);
+        }
+
+        protected override void OnStart()
+        {
+            base.OnStart();
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+        }
+
+        /// <summary>
+        /// Activity is backgrounded
+        /// </summary>
+        protected override void OnStop()
+        {
+            base.OnStop();
+        }
+
+        /// <summary>
+        /// Activity is no more in background
+        /// </summary>
+        protected override void OnRestart()
+        {
+            base.OnRestart();
+        }
+
+
+        protected override void OnPause()
+        {
+            base.OnPause();
+        }
+
+        protected override void OnDestroy()
+        {
+            base.OnDestroy();
+        }
+
+
     }
 }
