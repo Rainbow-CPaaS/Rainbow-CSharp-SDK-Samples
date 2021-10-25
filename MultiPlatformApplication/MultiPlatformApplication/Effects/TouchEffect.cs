3016664750e3e92ebe88c81ac1e4c60a313f41ff
Christophe Irles <christophe.irles@al-enterprise.com>
2021-10-25 15:39:00 +0200

    [MultiPlatform] TouchEffect: Add Command as bindable property

--- MultiPlatformApplication/MultiPlatformApplication/Effects/TouchEffect.cs
+++ MultiPlatformApplication/MultiPlatformApplication/Effects/TouchEffect.cs
@@ -1,11 +1,42 @@
 ﻿using MultiPlatformApplication.Events;
+using MultiPlatformApplication.Helpers;
 using System;
+using System.Windows.Input;
 using Xamarin.Forms;
 
 namespace MultiPlatformApplication.Effects
 {
     public class TouchEffect : RoutingEffect
     {
+
+#region  ValidationCommand Property
+
+        public static readonly BindableProperty CommandProperty = BindableProperty.Create("Command", typeof(ICommand), typeof(TouchEffect), null, propertyChanged: OnCommandChanged);
+
+        public static ICommand GetCommand(BindableObject view)
+        {
+            return (ICommand)view.GetValue(CommandProperty);
+        }
+
+        public static void SetCommand(BindableObject view, ICommand value)
+        {
+            view.SetValue(CommandProperty, value);
+        }
+
+        static void OnCommandChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            var view = bindable as View;
+            if (view == null)
+                return;
+
+            ICommand command = (ICommand)newValue;
+            if (command != null)
+                Helper.AddEffect(view, new TouchEffect());
+        }
+
+#endregion ValidationCommand Property
+
+
         // Based on: https://github.com/xamarin/xamarin-forms-samples/tree/main/Effects/TouchTrackingEffect
 
         public event EventHandler<TouchActionEventArgs> TouchAction;
@@ -19,6 +50,15 @@ namespace MultiPlatformApplication.Effects
         public void OnTouchAction(Element element, TouchActionEventArgs args)
         {
             TouchAction?.Invoke(element, args);
+
+            if (args.Type == TouchActionType.Pressed)
+            {
+                if(element.GetValue(CommandProperty) is ICommand iCommand)
+                {
+                    if (iCommand?.CanExecute(element) == true)
+                        iCommand.Execute(element);
+                }
+            }
         }
     }
 
