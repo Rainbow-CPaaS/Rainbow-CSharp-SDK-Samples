f34dcca1a52016a9bc8fc3a84997c7a3d4865098
ChristopheI <christophe.irles@al-enterprise.com>
2022-12-12 10:26:17 +0100

    BotAdaptiveCards: update code to use new Rainbow.SDK

--- Bot/BotAdaptiveCards/RainbowApplicationInfo.cs
+++ Bot/BotAdaptiveCards/RainbowApplicationInfo.cs
@@ -11,6 +11,8 @@ namespace BotAdaptiveCards
         // NLog configuration file used to log SDK debug information
         static internal readonly String NLOG_CONFIG_FILE_PATH = @"..\..\..\..\..\NLogConfiguration.xml";
 
+        static internal readonly Boolean USE_ALWAYS_SAME_ADAPTIVE_CARD = true;
+
         // DEFINE YOUR APP_ID,  APP_SECRET_KEY,  HOST_NAME etc ...
         static internal readonly string APP_ID = DEFAULT_VALUE;
         static internal readonly string APP_SECRET_KEY = DEFAULT_VALUE;
@@ -22,6 +24,5 @@ namespace BotAdaptiveCards
         static internal readonly string PASSWORD_BOT = DEFAULT_VALUE;
 
         static internal readonly List<String> USER_LIST_FOR_MCQ = new List<String>() { "email1@myCompany.com", "email2@myCompany.com" };
-
     }
 }
