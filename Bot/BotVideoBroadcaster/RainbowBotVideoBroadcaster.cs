ed926353f9580906492813fd29bd9d9d3489897a
ChristopheI <christophe.irles@al-enterprise.com>
2024-01-25 12:02:02 +0100

    [BotVideoBroadcaster] Use last SDK and .Net 8.0

--- Bot/BotVideoBroadcaster/RainbowBotVideoBroadcaster.cs
+++ Bot/BotVideoBroadcaster/RainbowBotVideoBroadcaster.cs
@@ -941,7 +941,7 @@ namespace BotVideoBroadcaster
         private void RbApplication_ConnectionStateChanged(object? sender, ConnectionStateEventArgs e)
         {
 
-            switch (e.State)
+            switch (e.ConnectionState.State)
             {
                 case ConnectionState.Connected:
                     FireTrigger(Trigger.Connect);
