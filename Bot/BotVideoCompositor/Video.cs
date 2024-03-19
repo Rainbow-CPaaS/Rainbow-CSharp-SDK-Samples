5a762245f0a39cb63fe0d7cd7578c803fe72e9e5
ChristopheI <christophe.irles@al-enterprise.com>
2024-03-19 11:00:02 +0100

    BotVideoCompositor: Sample update to use last SDK version

---
+++ Bot/BotVideoCompositor/Video.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BotVideoCompositor
+{
+    public class Video
+    {
+        public String Uri { get; set; }
+
+        public Dictionary<String, String> Settings { get; set; }
+
+        public Video() 
+        {
+            Uri = "";
+            Settings = new Dictionary<String, String>();
+        }
+    }
+}
