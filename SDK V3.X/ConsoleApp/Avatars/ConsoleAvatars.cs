b65ac5a26710989b8f6e63b959a5909696bf26ad
Christophe Irles <christophe.irles@al-enterprise.com>
2025-04-24 09:41:22 +0200

    [SDK V3.X] ConsoleApp: Add Avatars example

---
+++ SDK V3.X/ConsoleApp/Avatars/ConsoleAvatars.cs
@@ -0,0 +1,306 @@
+﻿
+using Rainbow;
+using Rainbow.Consts;
+
+using Rainbow.Example.Common;
+using Util = Rainbow.Example.Common.Util;
+using Rainbow.SimpleJSON;
+using Rainbow.Common;
+
+// --------------------------------------------------
+
+ExeSettings? exeSettings = null;
+Credentials? credentials = null;
+
+if ((!ReadExeSettings()) || (exeSettings is null))
+    return;
+
+if ((!ReadCredentials()) || (credentials is null))
+    return;
+
+Util.WriteRed($"Account used: [{credentials.UsersConfig[0].Login}]");
+
+// --------------------------------------------------
+
+Object consoleLockObject = new(); // To lock until the current console display is performed
+String CR = Rainbow.Util.CR; // Get carriage return;
+
+// In "exeSettings.json" using "logFolderPath" property, we defined a folder where the logs must be stored
+String logFolderPath = exeSettings.LogFolderPath;
+
+// In "credentials.json" using "userConfig" object, we defined a prefix used as logger prefix (this prefix permits to have logs stored in specific file for this "userConfig")
+String logPrefix = credentials.UsersConfig[0].Prefix;
+
+// Using NLogConfigurator, we specify the folder where log will be stored
+NLogConfigurator.Directory = logFolderPath;
+
+// Using NLogConfigurator, we add a logger using the preix
+NLogConfigurator.AddLogger(logPrefix);
+
+Rainbow.Util.SetLogAnonymously(false);
+
+// Create Rainbow SDK objects
+var RbApplication = new Application(iniFolderFullPathName: logFolderPath, iniFileName: logPrefix +".ini", loggerPrefix: logPrefix);
+var RbAutoReconnection = RbApplication.GetAutoReconnection();
+var RbBubbles = RbApplication.GetBubbles();
+
+// ----------------------------------------
+// START - Code specific about Avatars service
+
+// Create Avatars service
+var RbAvatars = Avatars.GetOrCreateInstance(RbApplication);
+
+// Specify where Avatars will be stored
+RbAvatars.SetFolderPathUsedToStoreAvatars(Path.Combine(".", "avatars"));
+
+// Specify which Avatar's kinds we want to manage - here we want Contact, Bubble and Channel
+RbAvatars.SetAvatarCategories(Avatars.AvatarCategory.CONTACT, Avatars.AvatarCategory.BUBBLE, Avatars.AvatarCategory.CHANNEL); ;
+
+// Initialize the service - mainly Folders to store each Avatar Category are created
+if (!RbAvatars.Initialize())
+    Util.WriteRed("CANNOT initialize Avatars service ...");
+
+// Indicate that the download can be done
+RbAvatars.AllowAvatarDownload(true);
+
+// We want to know when a new Avatar has been updated / downloaded
+RbAvatars.PeerAvatarUpdated += RbAvatars_PeerAvatarUpdated;
+
+// END - Code specific about Avatars service
+// ----------------------------------------
+
+
+// We want to receive events from SDK
+RbApplication.AuthenticationFailed += RbApplication_AuthenticationFailed;       // Triggered when the authentication process will fail
+RbApplication.AuthenticationSucceeded += RbApplication_AuthenticationSucceeded; // Triggered when the authentication process will succeed
+RbApplication.ConnectionStateChanged += RbApplication_ConnectionStateChanged;   // Triggered when the Connection State will change
+
+RbAutoReconnection.Cancelled += RbAutoReconnection_Cancelled;                       // Triggered when AutoReonnection service is cancelled
+RbAutoReconnection.Started += RbAutoReconnection_Started;                           // Triggered when AutoReonnection service is started
+RbAutoReconnection.MaxNbAttemptsReached += RbAutoReconnection_MaxNbAttemptsReached; // Triggered when AutoReonnection service reachde the mawimun number of attempts tryig to connect to the server
+RbAutoReconnection.TokenExpired += RbAutoReconnection_TokenExpired;                 // Triggered when the Security Token is expired
+
+RbBubbles.BubbleAffiliationsPerformed += RbBubbles_BubbleAffiliationsPerformed;
+
+// Set global configuration info
+RbApplication.SetApplicationInfo(credentials.ServerConfig.AppId, credentials.ServerConfig.AppSecret);
+RbApplication.SetHostInfo(credentials.ServerConfig.HostName);
+
+Util.WriteGreen($"{CR}Use [ESC] at anytime to quit");
+
+Util.WriteGreen($"{CR}Use [Q] at anytime once loggued to quit using Logout");
+Util.WriteGreen($"Use [A] at anytime once loggued to cancel/stop AutoReconnection service");
+// Start login
+Util.WriteWhite($"{CR}Starting login ...");
+var _ = RbApplication.LoginAsync(credentials.UsersConfig[0].Login, credentials.UsersConfig[0].Password);
+do
+{
+    await Task.Delay(200);
+    await CheckInputKey();
+} while (true);
+
+async Task CheckInputKey()
+{
+    while (Console.KeyAvailable)
+    {
+        var userInput = Console.ReadKey(true);
+
+        switch (userInput.Key)
+        {
+            case ConsoleKey.Escape:
+                Util.WriteYellow($"Asked to end process using [ESC] key");
+                Environment.Exit(0);
+                return;
+
+            case ConsoleKey.Q:
+                if(RbApplication.IsConnected())
+                {
+                    // We logout - here we don't wait the result
+                    var taskSdkResult = await RbApplication.LogoutAsync();
+                    if (taskSdkResult.Success)
+                    {
+                        // Nothing to do here - on success, event Cancelled from AutoReconnexion is triggered
+                    }
+                    else
+                        Util.WriteRed($"LogoutAsync - Error:[{taskSdkResult.Result}]");
+                }
+                return;
+
+
+            case ConsoleKey.A:
+                if (RbAutoReconnection.IsStarted)
+                {
+                    Util.WriteRed($"{CR}AutoReconnection - Service cancelled/Stopped");
+                    RbAutoReconnection.Cancel();
+                }
+                return;
+        }
+    }
+}
+
+#region Events received from the SDK
+
+
+void RbAvatars_PeerAvatarUpdated(Rainbow.Model.Peer peer)
+{
+    String path;
+    switch(peer.Type)
+    {
+        case Rainbow.Consts.EntityType.User:
+            path = RbAvatars.GetContactAvatarPath(peer.Id);
+            Util.WriteBlue($"{CR}New Avatar available for Contact: [{peer.ToString(DetailsLevel.Small)}] - Path:[{path}]");
+            break;
+
+        case Rainbow.Consts.EntityType.Bubble:
+            path = RbAvatars.GetBubbleAvatarPath(peer.Id);
+            Util.WriteBlue($"{CR}New Avatar available for Bubble: [{peer.ToString(DetailsLevel.Small)}] - Path:[{path}]");
+            break;
+
+        case Rainbow.Consts.EntityType.Channel:
+            path = RbAvatars.GetChannelAvatarPath(peer.Id);
+            Util.WriteBlue($"{CR}New Avatar available for Channel: [{peer.ToString(DetailsLevel.Small)}] - Path:[{path}]");
+            break;
+    }
+}
+
+void RbApplication_ConnectionStateChanged(Rainbow.Model.ConnectionState connectionState)
+{
+    // Display the CurrentNbAttempts
+    if (connectionState.Status == ConnectionStatus.Connecting)
+        Util.WriteYellow($"{CR}AutoReconnection.CurrentNbAttempts: [{RbAutoReconnection.CurrentNbAttempts}]");
+
+    // We log connection state in the console - use differnte color according the status
+    String color;
+    switch (connectionState.Status)
+    {
+        case ConnectionStatus.Connected:
+            color = Util.BLUE;
+            break;
+
+        case ConnectionStatus.Disconnected:
+            color = Util.RED;
+            break;
+
+        case ConnectionStatus.Connecting:
+        default:
+            color = Util.GREEN;
+            break;
+
+    }
+    Util.WriteToConsole($"{CR}{color}Event Application.ConnectionStateChanged triggered - Connection Status: [{connectionState.Status}]");
+
+    // If we are disconnected and the AutoReconnection is stopped, nothing more wille happpen
+    // So we quit the process
+    if ((connectionState.Status == ConnectionStatus.Disconnected) && (!RbAutoReconnection.IsStarted))
+    {
+        Util.WriteYellow($"{CR}We quit the process since AutoReconnection is stopped and we are disconnected");
+        Environment.Exit(0);
+    }
+}
+
+void RbApplication_AuthenticationSucceeded()
+{
+    // Authentication Succeeded- we display in the console the info
+    Util.WriteBlue($"{CR}Event Application.AuthenticationSucceeded triggered");
+}
+
+void RbApplication_AuthenticationFailed(SdkError sdkError)
+{
+    // Authentication failed - we display in the console the reason
+    Util.WriteRed($"{CR}Event Application.AuthenticationFailed triggered - SdkError:{sdkError}");
+}
+
+void RbBubbles_BubbleAffiliationsPerformed()
+{
+    // Bubble Affiliations Performed - we display in the console the info
+    Util.WriteBlue($"{CR}Event Bubbles_BubbleAffiliationsPerformed triggered");
+
+}
+
+void RbAutoReconnection_TokenExpired()
+{
+    Util.WriteRed($"{CR}Event AutoReconnection.TokenExpired triggered");
+}
+
+void RbAutoReconnection_MaxNbAttemptsReached()
+{
+    Util.WriteRed($"{CR}Event AutoReconnection.MaxNbAttemptsReached triggered");
+}
+
+void RbAutoReconnection_Started()
+{
+    Util.WriteBlue($"{CR}Event AutoReconnection.Started triggered");
+}
+
+void RbAutoReconnection_Cancelled(SdkError sdkError)
+{
+    if (sdkError.Type == Rainbow.Enums.SdkErrorType.NoError)
+    {
+        // The service has been cancelled/stopped voluntarily
+        Util.WriteYellow($"{CR}Event AutoReconnection.Cancelled triggered - Done using the SDK voluntarily");
+    }
+    else 
+    {
+        // The service has been cancelled/stopped involuntarily - display the reason
+        Util.WriteBlue($"{CR}Event AutoReconnection.Cancelled triggered - SdkError(Exception]:[{sdkError}]");
+    }
+
+    Util.WriteWhite($"{CR}We quit since the AutoReconnection has been Cancelled");
+    Environment.Exit(0);
+}
+
+#endregion Events received from the SDK
+
+Boolean ReadExeSettings()
+{
+    String exeSettingsFilePath = $".{Path.DirectorySeparatorChar}config{Path.DirectorySeparatorChar}exeSettings.json";
+    if (!File.Exists(exeSettingsFilePath))
+    {
+        Util.WriteRed($"The file '{exeSettingsFilePath}' has not been found.");
+        return false;
+    }
+
+    String jsonConfig = File.ReadAllText(exeSettingsFilePath);
+    var jsonNode = JSON.Parse(jsonConfig);
+
+    if ((jsonNode is null) || (!jsonNode.IsObject))
+    {
+        Util.WriteRed($"Cannot get JSON data from file '{exeSettingsFilePath}'.");
+        return false;
+    }
+
+    if (ExeSettings.FromJsonNode(jsonNode["exeSettings"], out exeSettings))
+    {
+        // Set where log files must be stored
+        NLogConfigurator.Directory = exeSettings.LogFolderPath;
+    }
+    else
+    {
+        Util.WriteRed($"Cannot read 'exeSettings' object OR invalid/missing data - file:'{exeSettingsFilePath}'.");
+        return false;
+    }
+
+    return true;
+}
+
+Boolean ReadCredentials(string fileName = "credentials.json")
+{
+    var credentialsFilePath = $".{Path.DirectorySeparatorChar}config{Path.DirectorySeparatorChar}{fileName}";
+    if (!File.Exists(credentialsFilePath))
+    {
+        Util.WriteRed($"The file '{credentialsFilePath}' has not been found.");
+        return false;
+    }
+
+    String jsonConfig = File.ReadAllText(credentialsFilePath);
+    var jsonNode = JSON.Parse(jsonConfig);
+
+    if (!Credentials.FromJsonNode(jsonNode["credentials"], out credentials))
+    {
+        Util.WriteRed($"Cannot read 'credentials' object OR invalid/missing data in file:[{fileName}].");
+        return false;
+    }
+
+    return true;
+}
+
---
+++ SDK V3.X/ConsoleApp/Avatars/README.md
@@ -0,0 +1,34 @@
+![Rainbow](./../../../logo_rainbow.png)
+
+# Rainbow CSharp SDK v3 - Avatars
+
+This console application permits to understand how to use the package [Rainbow.CSharp.SDK.Common](https://www.nuget.org/packages/Rainbow.CSharp.SDK.Common).
+
+Documentation is available [here](https://developers.openrainbow.com/doc/sdk/csharp/common/lts/guides/001_getting_started)
+
+## Prerequisites
+
+### Rainbow API HUB
+
+This SDK is using the [Rainbow environment](https://developers.openrainbow.com/)
+ 
+This environment is based on the [Rainbow service](https://www.openrainbow.com/) 
+
+### Rainbow CSharp SDK
+
+To have more info about the SDK:
+- check [Getting started guide](https://developers.openrainbow.com/doc/sdk/csharp/core/lts/guides/001_getting_started)
+- check [API documentation](https://developers.openrainbow.com/doc/sdk/csharp/core/lts/api/Rainbow.Application)
+
+## Features
+- Download automatically Avatars of Contacts, Bubbles and Channels known by the user connected
+- The download is done only if avatars are not yet available locally.
+- If a Peer modified an Avatar, it will be automatically downloaded too.
+
+## File exeSettings.json
+
+You need to set correctly the file "exeSettings.json" like described in chapter [File exeSettings.json](./../../ConfigurationFiles.md#exeSettings.json) - no need here to use external dependencies.
+
+## File credentials.json
+
+You need to set correctly the file "credentials.json" like described in chapter [File credentials.json](./../../ConfigurationFiles.md#credentials.json).
