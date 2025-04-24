da258f0a8d71b9572a3e9607ea82a9a4fe7331a9
Christophe Irles <christophe.irles@al-enterprise.com>
2025-04-24 09:45:26 +0200

    [SDK V3.X] Common: use SDK (core) v3.2.0

--- SDK V3.X/Common/Util.cs
+++ SDK V3.X/Common/Util.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Rainbow.Example.Common
+﻿namespace Rainbow.Example.Common
 {
     /// <summary>
     /// Static class with some utility methods
