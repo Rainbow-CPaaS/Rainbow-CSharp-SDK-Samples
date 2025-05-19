c2bcc27d61094f4c0feb2439c810aa6911c866f1
Christophe Irles <christophe.irles@al-enterprise.com>
2025-05-19 10:17:26 +0200

    [SDK V3.X] Add StyledLabel, EmojiButton a,d EmojiButtonsPanel

---
+++ SDK V3.X/TerminalApp/TerminalAppLibrary/View/EmojiButton.cs
@@ -0,0 +1,120 @@
+﻿using Terminal.Gui;
+
+public class EmojiButton : View
+{
+    private readonly Object lockDisplay = new();
+
+    private readonly String id;
+
+    private readonly String emoji;
+    private Boolean selected = false;
+    private Boolean mouseOver = false;
+
+    private readonly Label lbl;
+    private readonly ColorScheme colorSchemeDefault;
+    private readonly ColorScheme colorSchemeSelected;
+    private readonly ColorScheme colorSchemeOver;
+
+    public event EventHandler<String>? ButtonClick;
+
+    public EmojiButton(String id, String emoji, int width, Boolean selected, ColorScheme colorSchemeDefault, ColorScheme colorSchemeSelected, ColorScheme colorSchemeOver)
+    {
+        this.id = id;
+        this.emoji = $" {emoji} ";
+        this.colorSchemeDefault = colorSchemeDefault;
+        this.colorSchemeSelected = colorSchemeSelected;
+        this.colorSchemeOver = colorSchemeOver;
+        this.selected = selected;
+
+        lbl = new()
+        {
+            Width = width,
+            Height = 1,
+            Text = this.emoji,
+            TextAlignment = Alignment.Center,
+            CanFocus = true,
+            ColorScheme = colorSchemeDefault
+        };
+
+        lbl.MouseClick += EmojiButton_MouseClick;
+        lbl.MouseEnter += EmojiButton_MouseEnter;
+        lbl.MouseLeave += EmojiButton_MouseLeave;
+
+        Add(lbl);
+
+        Width = width;
+        Height = 1;
+        CanFocus = true;
+
+        UpdateDisplay();
+    }
+
+    public Boolean Selected
+    {
+        get => selected;
+        set
+        {
+            if (value != selected)
+            {
+                selected = value;
+                UpdateDisplay();
+            }
+        }
+    }
+
+    private void UpdateDisplay()
+    {
+        lock (lockDisplay)
+        {
+            if (selected)
+            {
+                lbl.Text = $"[{emoji}]";
+                lbl.ColorScheme = colorSchemeSelected;
+                ColorScheme = colorSchemeSelected;
+            }
+            else
+            {
+                if (mouseOver)
+                {
+                    lbl.Text = $"_{emoji}_";
+                    lbl.ColorScheme = colorSchemeOver;
+                    ColorScheme = colorSchemeOver;
+                }
+                else
+                {
+                    lbl.Text = emoji;
+                    lbl.ColorScheme = colorSchemeDefault;
+                    ColorScheme = colorSchemeDefault;
+                }
+            }
+            //lbl.SetNeedsDraw();
+            //SetNeedsDraw();
+        }
+    }
+
+    private void EmojiButton_MouseLeave(object? sender, EventArgs e)
+    {
+        mouseOver = false;
+        UpdateDisplay();
+    }
+
+    private void EmojiButton_MouseEnter(object? sender, System.ComponentModel.CancelEventArgs e)
+    {
+        mouseOver = true;
+        UpdateDisplay();
+    }
+
+    private void EmojiButton_MouseClick(object? sender, MouseEventArgs e)
+    {
+        if (e.Flags == MouseFlags.Button1Clicked)
+        {
+            e.Handled = true;
+            if (selected) return; // nothing to do
+
+            selected = true;
+            UpdateDisplay();
+            ButtonClick?.Invoke(this, id);
+        }
+    }
+}
+
---
+++ SDK V3.X/TerminalApp/TerminalAppLibrary/View/EmojiButtonsPanel.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terminal.Gui;
+
+public class EmojiButtonsPanel: View
+{
+    const int BTN_WIDTH = 6;
+    private String? selected = null;
+    private int width = 0;
+    private Dictionary<String, EmojiButton> emojiButtonsList;
+    private EmojiButton? lastEmojiButton;
+
+    private readonly ColorScheme colorSchemeDefault;
+    private readonly ColorScheme colorSchemeSelected;
+    private readonly ColorScheme colorSchemeOver;
+
+    public event EventHandler<String>? SelectionUpdated;
+
+    public EmojiButtonsPanel(ColorScheme? colorSchemeDefault = null, ColorScheme? colorSchemeSelected = null, ColorScheme? colorSchemeOver = null)
+    {
+        colorSchemeDefault ??= Tools.ColorSchemeMain;
+        colorSchemeSelected ??= Tools.ColorSchemeBlackOnWhite;
+        colorSchemeOver ??= Tools.ColorSchemeBlueOnGray;
+
+        emojiButtonsList = new();
+
+        lastEmojiButton = null;
+
+        Width = width;
+        Height = 1;
+
+        CanFocus = true;
+    }
+
+    public void SetVisible(string id, Boolean visible)
+    {
+        if (emojiButtonsList.TryGetValue(id, out var button))
+        {
+            if(visible && button.Width != BTN_WIDTH)
+            {
+                button.Width = BTN_WIDTH;
+                
+                width += BTN_WIDTH;
+                Width = width;
+
+                SetNeedsDraw();
+            }
+            else if ((!visible) && button.Width != 0)
+            {
+                button.Width = 0;
+                width -= BTN_WIDTH;
+                Width = width;
+
+                SetNeedsDraw();
+            }
+        }
+    }
+
+    public String Selected
+    {
+        get => selected;
+        set
+        {
+            if(value != selected)
+            {
+                foreach(var button in emojiButtonsList)
+                {
+                    button.Value.Selected = (button.Key == value);
+                }
+                selected = value;
+            }
+        }
+    }
+
+    public Boolean Add(String id, String emoji, int width = BTN_WIDTH, Boolean isSelected = false)
+    {
+        if (emojiButtonsList.ContainsKey(id))
+            return false;
+
+        this.width += width;
+        Width = this.width;
+
+        if (isSelected)
+            selected = id;
+
+        EmojiButton emojiButton = new(id, emoji, width, isSelected, colorSchemeDefault, colorSchemeSelected, colorSchemeOver);
+        emojiButton.X = lastEmojiButton is null ? 0 : Pos.Right(lastEmojiButton);
+
+        emojiButton.ButtonClick += EmojiButton_ButtonClick;
+
+        Add(emojiButton);
+        emojiButtonsList.Add(id, emojiButton);
+
+        lastEmojiButton = emojiButton;
+
+        
+        return true;
+    }
+
+    private void EmojiButton_ButtonClick(object? sender, string e)
+    {
+        foreach (var button in emojiButtonsList)
+        {
+            if (button.Key != e)
+                button.Value.Selected = false;
+        }
+        if (e != selected)
+        {
+            selected = e;
+            SelectionUpdated?.Invoke(this, selected);
+        }
+    }
+}
